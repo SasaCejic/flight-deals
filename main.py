13f23438ed32346e031e48b4c7d51fcf86526822
SasaCejic <cejicsasa17@gmail.com>
2025-09-27 16:53:46 +0200

    Made first API call for flight destinations

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import os
+import requests
+
+AMADEUS_API_KEY = os.environ["AMADEUS_API_KEY"]
+AMADEUS_API_SECRET = os.environ["AMADEUS_API_SECRET"]
+AMADEUS_TOKEN = os.environ["AMADEUS_TOKEN"]
+
+amadeus_url = "https://test.api.amadeus.com/v1/shopping/flight-destinations"
+
+amadeus_headers = {
+    "Authorization": f"Bearer {AMADEUS_TOKEN}"
+
+}
+amadeus_params = {
+    "origin": "PAR",
+    "maxPrice": 200
+}
+
+response = requests.get(url=amadeus_url, params=amadeus_params, headers=amadeus_headers)
+
+print(response.text)
